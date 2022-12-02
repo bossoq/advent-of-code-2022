aa95caadb551edadab57f6e5c6a561df904b8cd9
Kittipos Wajakajornrit <boss.zz@me.com>
2022-12-02 12:08:41 +0700

    Add day 01

---
+++ 01/solution1.py
@@ -0,0 +1,13 @@
+import sys
+
+
+with open(sys.argv[1], 'r') as f:
+    data = [0]
+    for line in f:
+        if line.strip() == '':
+            data.append(0)
+        else:
+            data[-1] += int(line.strip())
+    data.sort()
+
+print(data[-1])
